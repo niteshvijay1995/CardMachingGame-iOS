ea307ef1c44f3573a285218e35aa8225ef7edcf5
nitesh.vi <nitesh.vi@client.openvpn.net>
2017-08-08 15:08:41 +0530

    Initial Commit

---
+++ CardMachingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardMachingGame
+//
+//  Created by nitesh.vi on 08/08/17.
+//  Copyright © 2017 nitesh.vi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
